1fb6c6a98c587d161b2a871aa0556505b0aa3285
YuriyOhii <uaogiy@gmail.com>
2023-10-24 16:04:43 +0300

    adds itemDetails page

--- src/components/QuizItem/QuizItem.jsx
+++ src/components/QuizItem/QuizItem.jsx
@@ -1,43 +1,17 @@
-import {
-  Quiz,
-  DelQuizBtn,
-  InfoWrapper,
-  Info,
-  Topic,
-  CloseModalBtn,
-} from './QuizItem.styled';
+import { Quiz, DelQuizBtn, InfoWrapper, Info, Topic } from './QuizItem.styled';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
 import { BsTrash3Fill } from 'react-icons/bs';
-import Modal from 'react-modal';
-
-const customStyles = {
-  content: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
+import { Link } from 'react-router-dom';
 
 export const QuizItem = ({
   item: { level, topic, time, questions, id },
   onClick,
 }) => {
-  const [ isModalOpen, setIsModalOpen ] = useState(false);
-
-  const openModal = () => setIsModalOpen(true);
-
-  const closeModal = () => setIsModalOpen(false);
-
-  Modal.setAppElement('#root');
-
   return (
     <Quiz $level={level}>
-      <Topic onClick={openModal}>{topic}</Topic>
+      <Link to={`/quizzes/${id}`}>
+        <Topic>{topic}</Topic>
+      </Link>
       <InfoWrapper>
         <Info $color="red">Level: {level}</Info>
         <Info $color="blue">Time: {time}min</Info>
@@ -46,16 +20,6 @@ export const QuizItem = ({
       <DelQuizBtn type="button" onClick={() => onClick(id)}>
         <BsTrash3Fill />
       </DelQuizBtn>
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={closeModal}
-        style={customStyles}
-        contentLabel="Example Modal"
-      >
-        <h2>{topic}</h2>
-        <CloseModalBtn onClick={closeModal}>close</CloseModalBtn>
-        <div>I am a modal</div>
-      </Modal>
     </Quiz>
   );
 };
--- src/pages/QuizDetails.jsx
+++ src/pages/QuizDetails.jsx
@@ -1,3 +1,60 @@
+import { useParams } from 'react-router-dom';
+import { getQuizeById } from '../components/services/api';
+import { useState, useEffect } from 'react';
+import { toast } from 'react-hot-toast';
+import { Audio } from 'react-loader-spinner';
+import { ErrorNotice } from '../components/Error';
+
 export default function QuizDetails() {
-  return <div>QuizDetails</div>;
+  const { quizId } = useParams();
+  const [quiz, setQuiz] = useState(0);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getDetails = async () => {
+      try {
+        setError(null);
+        setLoading(true);
+        const quizItem = await getQuizeById(quizId);
+        if (quizItem) {
+          setQuiz(quizItem);
+          toast.success('Fetched successfully!');
+        }
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getDetails();
+  }, [quizId]);
+
+  return (
+    <div>
+      {' '}
+      {loading && (
+        <div>
+          <Audio
+            height="80"
+            width="80"
+            radius="9"
+            color="black"
+            ariaLabel="loading"
+          />
+        </div>
+      )}
+      {error && <ErrorNotice errorMessage={error.message} />}
+      <h1>Topic: {quiz.topic}</h1>
+      <p>
+        <b>Level</b> {quiz.level}
+      </p>
+      <p>
+        <b>Time</b> {quiz.time}
+      </p>
+      <p>
+        <b>Questions</b> {quiz.questions}
+      </p>
+    </div>
+  );
 }
